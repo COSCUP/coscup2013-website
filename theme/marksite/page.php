15f0acde22ef3885992463ec39eba3bccf1a8db6
Evelyn Hung <jj.evelyn@gmail.com>
2013-04-30 16:41:03 +0800

    add X-UA-Compatible

--- theme/marksite/page.php
+++ theme/marksite/page.php
@@ -20,6 +20,7 @@ switch($this->current[0])
 <html lang="<?php echo $lc->lang ?>">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title><?php echo $title; ?> | 2013 COSCUP-Open x [Web | Mobile | Data]</title>
 
 <meta name="keywords" content="">
