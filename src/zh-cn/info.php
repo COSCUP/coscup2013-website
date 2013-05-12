6c33e8e2165b16ecbc7546bf98cd9752aaac18c7
Evelyn Hung <jj.evelyn@gmail.com>
2013-05-13 00:33:03 +0800

    add zh-cn to-be-translated version

---
+++ src/zh-cn/info.php
@@ -0,0 +1,15 @@
+<?php
+$contents = array(
+	"index" => "首頁",
+	//"http://registrano.com/events/coscup2013-regist" => "報名",
+	"about" => "活動簡介",
+	"program" => "議程",
+	"venue" => "地點",
+	"sponsors" => "贊助單位",
+  "news" => "News",
+	"http://blog.coscup.org/" => "部落格",
+	"contact" => "聯絡資訊",
+  "openwall" => "Open"
+);
+
+$menu_hidden = array("index", "openwall");
