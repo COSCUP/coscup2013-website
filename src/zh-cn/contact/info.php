bf4a06e50ff5b66253055a7d3ce1a68f99b8613e
chusiang <chusiang.lai@gmail.com>
2013-07-11 00:44:18 +0800

    remove irc embedded tag.

--- src/zh-cn/contact/info.php
+++ src/zh-cn/contact/info.php
@@ -4,9 +4,3 @@ $contents = array(
 );
 
 $menu_hidden = array("index");
-
-# Embedded IRC ifame.
-echo "
-<div style='text-align:center; vertical-align:middle;'>
-	<iframe src='http://webchat.freenode.net?channels=coscup&uio=d4' width='900' height='600' style='border: 0;'></iframe>
-</div>";
